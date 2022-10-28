22b6138ffebc76e9a717e36daef328b1eac7d50d
최은총 <dmsvk0217@gmail.com>
2022-10-28 16:19:02 +0900

    react폴더 git 삭제

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname));
-
 app.listen(8080, function(){
   console.log('listening on 8080');
 });
 
-app.use(express.json());
-var cors = require('cors');
-app.use(cors());
+app.use(express.static(path.join(__dirname, 'react_app/build')));
+
+// app.use(express.json());
+// var cors = require('cors');
+// app.use(cors());
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'react_app/build/index.html'));
 });
 
-app.get('/dog', function(req, res){
-  res.sendFile(path.join(__dirname, 'react_app/build/dog.html'));
-});
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'react_app/build/index.html'));
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'react_app/public/index.html'));
+// });
