b2aa44ae7515043ee3b00548b3cb2b166917967c
최은총 <dmsvk0217@gmail.com>
2022-10-28 16:47:26 +0900

    first commit

--- server.js
+++ server.js
@@ -5,16 +5,8 @@ app.listen(8080, function(){
   console.log('listening on 8080');
 });
 
-app.use(express.static(path.join(__dirname, 'react_app/build')));
-
-// app.use(express.json());
-// var cors = require('cors');
-// app.use(cors());
+app.use(express.static(path.join(__dirname, '../front/build')));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'react_app/build/index.html'));
+  res.sendFile(path.join(__dirname, '../front/build/index.html'));
 });
-
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'react_app/public/index.html'));
-// });
