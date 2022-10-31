0d93da74b7c3d924a022834550a3f7a6074fc59b
dmsvk0217 <dmsvk0217@gmail.com>
2022-10-31 18:38:58 +0900

    password 암호화 with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,10 +31,25 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
-})
+});
 
+userSchema.pre("save", function (next) {
+  var user = this;
 
+  if (user.isModified("password")) {
+    //비밀번호 암호화 with bcrypt
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
 
-const User = mongoose.model('User', userSchema);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
--- server.js
+++ server.js
@@ -1,47 +1,54 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const badyParser = require('body-parser');
-const cors = require('cors');
+const badyParser = require("body-parser");
+const cors = require("cors");
 
-const config = require('./config/key');
-const { User } = require('./models/User');
+const config = require("./config/key");
+const { User } = require("./models/User");
 
 //mongoose 생성 및 연결
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-mongoose.connect(config.mongoURI, {
-  useUnifiedTopology: true,
-}).then(() => console.log('MonogeDB Conneted...'))
-  .catch(err => console.log(err));
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+mongoose
+  .connect(config.mongoURI, {
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MonogeDB Conneted..."))
+  .catch((err) => console.log(err));
 
 //서버 시작
-app.listen(8080, function(){
-  console.log('listening on 8080');
+app.listen(8080, function () {
+  console.log("listening on 8080");
 });
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'front/build')));
-app.use(bodyParser.urlencoded({extended: true})); //application/X-www-form-urlencoded
+app.use(express.static(path.join(__dirname, "front/build")));
+app.use(bodyParser.urlencoded({ extended: true })); //application/X-www-form-urlencoded
 app.use(bodyParser.json()); // application/json
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'front/build/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "front/build/index.html"));
 });
 
-app.post('/regster', function(req, res){
+app.post("/register", function (req, res) {
   const user = new User(req.body);
+
+  //console.log(req.body);
+
+  //user.save하기 전에 비밀번호 암호화하기.
   user.save((err, userInfo) => {
-    if(err) return res.json({success: false, err});
-    else return res.status(200).json({
-      success: true,
-      userInfo,
-    })
-  })
+    if (err) return res.json({ success: false, err });
+    else
+      return res.status(200).json({
+        success: true,
+        userInfo,
+      });
+  });
 });
 
 //경로가 서버에 개발되어있지 않은 경우 index.html로 라우팅.
-app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname, 'front/build/index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "front/build/index.html"));
 });
