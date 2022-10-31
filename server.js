822f87a038fdf4610871f17c008dba674d7b18cd
최은총 <dmsvk0217@gmail.com>
2022-10-31 13:00:26 +0900

    초기 세팅 완료

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.listen(8080, function(){
   console.log('listening on 8080');
 });
 
-app.use(express.static(path.join(__dirname, '../front/build')));
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'front/build')));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../front/build/index.html'));
+  res.sendFile(path.join(__dirname, 'front/build/index.html'));
+});
+
+//경로가 서버에 개발되어있지 않은 경우 index.html로 라우팅.
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, 'front/build/index.html'));
 });
