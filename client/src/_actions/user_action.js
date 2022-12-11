133079bc2435c2c3a37d6d98ad40b4f8b68ecc40
dmsvk0217 <dmsvk0217@gmail.com>
2022-12-11 22:55:33 +0000

    register 기능 redux 적용

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,15 +1,24 @@
 import axios from "axios";
-import { LOGIN_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER } from "./types";
 
 export function loginUser(dataToSubmit) {
   const request = axios
-    .post("/api/user/login", dataToSubmit, {
-      withCredentials: true,
-    })
+    .post("/api/user/login", dataToSubmit, { withCredentials: true })
     .then((response) => response.data);
 
   return {
     type: LOGIN_USER,
+    payload: request,
+  };
+}
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post("/api/user/register", dataToSubmit, { withCredentials: true })
+    .then((response) => response.data);
+
+  return {
+    type: REGISTER_USER,
     payload: request,
   };
 }
--- client/src/components/view/RegisterPage/RegisterPage.js
+++ client/src/components/view/RegisterPage/RegisterPage.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
 
 function Register() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -20,24 +23,20 @@ function Register() {
   };
 
   const registerHandler = function () {
-    /*
-    axios로 서버에게 register요청하기
-    */
-    axios // proxy 경로 적용이 왜 안되는지 모르겠음........ 적용이 안됐는데 왜 cors오류는 발생안하는지도 모르겠음......
-      .post("http://localhost:7777/api/user/register", {
-        name: name,
-        email: email,
-        password: password,
-      })
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
-        if (res.data.registerSuccess) {
-          // 회원가입성공 -> navigate를 사용하여 로그인 페이지로 라우팅
+    const dataToSubmit = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    dispatch(registerUser(dataToSubmit))
+      .then((response) => {
+        console.log(response);
+        console.log(response.payload);
+        if (response.payload.registerSuccess) {
           console.log("[register] 회원가입 성공입니다.");
           navigate("/login", { replace: true });
-        } else if (res.data.existUser) {
-          // 이미존재하는 -> ajax 경고 "이메일에 해당하는 계정이 이미 존재합니다."
+        } else if (response.data.existUser) {
           setalramText("이메일에 해당하는 계정이 이미 존재합니다");
         } else {
           // other cases
